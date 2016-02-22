ce79b02eb85d5d0113ff25f91f8d6a7fd5d26e53
AISSAT YACINE <3300425@ppti-14-308-16.ufr-info-p6.jussieu.fr>
2016-02-22 18:50:39 +0100

    fevrier

--- team.py
+++ team.py
@@ -2,11 +2,13 @@ import soccersimulator
 from soccersimulator import BaseStrategy, Vector2D, SoccerAction, settings
 from soccersimulator import SoccerTeam, SoccerMatch
 from soccersimulator import Player, SoccerTournament
-from strategies import attaquantcentral, defenseurcentral, defenseurgauche, defenseurdroit, Milieu, Milieudefensif
+from Strat import attaquant, defenseur_central, defenseur_gauche, defenseur_droit, milieu, milieu_defensif, attaquant_pointe
 
 
-team1 = SoccerTeam ("DZPOWER",[Player("Messi",attaquantcentral())])
+team1 = SoccerTeam ("DZPOWER",[Player("Messi",attaquant)])
 
-team2 = SoccerTeam ("DZPOWER",[Player("Raouf",attaquantcentral()), Player("Yacine",defenseurcentral())])
+team2 = SoccerTeam ("DZPOWER",[Player("Raouf",attaquant), Player("Yacine",defenseur_central)])
 
-team4 = SoccerTeam ("DZPOWER",[Player("Raouf",attaquantcentral()), Player("Yacine",Milieu()), Player("Raouf JR",Milieudefensif()), Player("Yacine JR",defenseurcentral())])
+team4 = SoccerTeam ("DZPOWER",[Player("Raouf",attaquant_pointe), Player("Yacine",milieu_defensif), Player("Raouf JR",milieu), Player("Yacine JR",defenseur_central)])
+
+
